127ec933841050e1ee1124b37d27f286146f5edf
connerjoseph <cradjoseph@gmail.com>
2025-07-08 14:39:56 +0100

    Add qr-image

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from "fs";
 
 inquirer
   .prompt([
@@ -8,7 +10,11 @@ inquirer
     }
   ])
   .then((answers) => {
-    console.log(answers);
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+ 
+var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
   })
   .catch((error) => {
     if (error.isTtyError) {
