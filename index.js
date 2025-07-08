cb261989529fad67abdd2741da219a915c14308f
connerjoseph <cradjoseph@gmail.com>
2025-07-08 14:59:30 +0100

    Add text file

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ inquirer
     var qr_svg = qr.image(url);
 qr_svg.pipe(fs.createWriteStream('qr_img.png'));
  
-var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
+fs.writeFile('URL.txt', url, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  }); 
   })
   .catch((error) => {
     if (error.isTtyError) {
