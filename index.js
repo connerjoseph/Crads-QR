c26e75eb1c2845423989a20d0ca90d09773ee8d4
connerjoseph <cradjoseph@gmail.com>
2025-07-09 11:47:04 +0100

    Add img and txt

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ fs.writeFile('URL.txt', url, (err) => {
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+
     } else {
-      // Something else went wrong
+
     }
   });
